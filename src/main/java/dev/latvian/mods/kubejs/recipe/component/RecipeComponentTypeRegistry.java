f24b35f841cac60eb5fb79549f45c19e563e2dee
Relentless <16513358+rlnt@users.noreply.github.com>
2026-09-15 13:39:21 +0200

    fix resource key components erasing their generic type (#1187)
    * fix resource key components erasing their generic type
    
    * add recipe component registration util function for record patterns

--- src/main/java/dev/latvian/mods/kubejs/recipe/component/RecipeComponentTypeRegistry.java
+++ src/main/java/dev/latvian/mods/kubejs/recipe/component/RecipeComponentTypeRegistry.java
@@ -3,6 +3,8 @@ package dev.latvian.mods.kubejs.recipe.component;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.resources.ResourceKey;
 
+import java.util.function.Function;
+
 public interface RecipeComponentTypeRegistry {
 	void register(ResourceKey<RecipeComponentType<?>> key, RecipeComponentType<?> type);
 
@@ -17,6 +19,10 @@ public interface RecipeComponentTypeRegistry {
 
 	default <CT extends RecipeComponent<?>> void unit(CT instance) {
 		register(instance.type(), () -> MapCodec.unit(instance));
+	}
+
+	default <K extends ResourceKey<RecipeComponentType<?>>, CT extends RecipeComponent<?>> void fromFactory(K key, Function<K, CT> factory) {
+		register(key, () -> MapCodec.unit(factory.apply(key)));
 	}
 
 	@SuppressWarnings("unchecked")
